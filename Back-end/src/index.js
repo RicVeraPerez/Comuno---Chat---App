3d7570bfe7f492ab2d08d78594d352ee07259cb4
Ricardo Vera <ricveraperez@gmail.com>
2025-07-20 00:53:46 -0400

    Man....

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -13,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "100mb" }));
@@ -21,10 +21,13 @@ app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === 'production'
+      ? 'https://comuno-chat-app-production.up.railway.app/'
+      : 'http://localhost:5173',
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -36,6 +39,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../Front-end", "dist", "index.html"));
   });
 }
+
+console.log("PORT env variable:", process.env.PORT);
+console.log("Listening on port:", PORT);
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
