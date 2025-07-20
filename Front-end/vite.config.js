7f57f1035d0d6332af711c6c8dc16a1cdeb4aad2
Ricardo Vera <ricveraperez@gmail.com>
2025-07-19 23:54:27 -0400

    Added proxy for Railway deploy

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Aquí le dices que todo lo que empiece con /api lo redirija al backend
+      "/api": {
+        target: "http://localhost:5001", // Cambia este puerto si tu backend corre en otro
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
