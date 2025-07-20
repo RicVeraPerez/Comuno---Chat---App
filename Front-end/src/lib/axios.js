510dcab979622ae9f464fb5ac111b4da5fa65ce0
Ricardo Vera <ricveraperez@gmail.com>
2025-07-20 00:17:56 -0400

    THE BUILD WORKED

--- Front-end/src/lib/axios.js
+++ Front-end/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE ===  "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : import.meta.env.VITE_API_URL,  // la URL completa del backend en producción
+  withCredentials: true,
+});
--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     proxy: {
       // Aquí le dices que todo lo que empiece con /api lo redirija al backend
